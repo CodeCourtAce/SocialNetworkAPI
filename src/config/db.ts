3fee6ac8eccb528ceb0a1fdd64f68610d0b0eb1d
Gianna DAconti <themeanmememan@gmail.com>
2024-12-10 21:11:32 -0500

    saving changes only changed 2 lines

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose';
-import { config } from './config';
+//import { config } from './config';
 
 const connectDB = async (): Promise<void> => {
   try {
-    await mongoose.connect(config.mongoUri);
+    //await mongoose.connect(config.mongoUri);
     console.log('MongoDB connected successfully');
   } catch (error) {
     console.error('MongoDB connection error:', error);
